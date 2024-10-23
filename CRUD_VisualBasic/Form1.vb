35a78516e4e66b09a6156a2321aa4a30863e2abe
MargelCastro <margelgabriel@gmail.com>
2024-10-22 23:09:31 -0600

    conexion completa

--- CRUD_VisualBasic/Form1.vb
+++ CRUD_VisualBasic/Form1.vb
@@ -1,4 +1,37 @@
-﻿Public Class Login
+﻿Imports System.Data.SqlClient
+
+Public Class Login
+
+    Dim conexion As New SqlConnection
+    Dim comando As New SqlCommand
+
+    Private Sub Login_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        conexion = New SqlConnection("server=REKEDOMINA;database = Gestor; integrated security = true")
+    End Sub
+
+    Private Sub btn_inicio_Click(sender As Object, e As EventArgs) Handles btn_inicio.Click
+        conexion.Open()
+        Dim consulta As String = "select * from Usuarios   where Loggin = '" & txt_user.Text & "'and Pass = '" & txt_pass.Text & "' "
+
+        comando = New SqlCommand(consulta, conexion)
+        Dim lector As SqlDataReader
+        lector = comando.ExecuteReader
+
+        If lector.HasRows Then
+            Me.Hide()
+            Admin_Task.ShowDialog()
+        Else
+            MessageBox.Show("Verifique los datos ingresados nuevamente...")
+        End If
+
+
+
+
+    End Sub
+
+
+
+
     Private Sub btn_inicio_MouseHover(sender As Object, e As EventArgs) Handles btn_inicio.MouseHover
         btn_inicio.BackColor = Color.Tomato
     End Sub
@@ -8,13 +41,7 @@
 
     End Sub
 
-    Private Sub btn_inicio_Click(sender As Object, e As EventArgs) Handles btn_inicio.Click
 
-    End Sub
-
-    Private Sub Login_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
 
     Private Sub txt_user_TextChanged(sender As Object, e As EventArgs) Handles txt_user.TextChanged
 
--- CRUD_VisualBasic/obj/Debug/net8.0-windows/CRUD_VisualBasic.AssemblyInfo.vb
+++ CRUD_VisualBasic/obj/Debug/net8.0-windows/CRUD_VisualBasic.AssemblyInfo.vb
@@ -16,7 +16,7 @@ Imports System.Reflection
 <Assembly: System.Reflection.AssemblyCompanyAttribute("CRUD_VisualBasic"),  _
  Assembly: System.Reflection.AssemblyConfigurationAttribute("Debug"),  _
  Assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0"),  _
- Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94b1e77ae45b47007a763065e3a7d8c6b673a12f"),  _
+ Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+263ab3b5800d0d98174a125ac7db78b898cc3de9"),  _
  Assembly: System.Reflection.AssemblyProductAttribute("CRUD_VisualBasic"),  _
  Assembly: System.Reflection.AssemblyTitleAttribute("CRUD_VisualBasic"),  _
  Assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0"),  _
