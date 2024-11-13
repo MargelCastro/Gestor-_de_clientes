e03c9c16c10f6fd790636cfb364cb2c8e092a9e8
Hans Blanco <hansblancodev@gmail.com>
2024-11-12 23:52:14 -0600

    Update DBGestor.sql

--- CRUD_VisualBasic/DB/DBGestor.sql
+++ CRUD_VisualBasic/DB/DBGestor.sql
@@ -4,24 +4,47 @@ create database Gestor
 go
 use Gestor
 go
-create table Usuarios(
-id int primary key identity not null,
-Nombre varchar (200) not null,
-Loggin varchar (100) not null unique,
-Pass varchar (50) not null,
-FechaRegistro DateTime not null,
-IdUsuarioActualizacion int not null,
-FechaActualizacion Date not null,
-Activo bit,
+CREATE TABLE usuario_de_aplicacion(
+	id INT PRIMARY KEY IDENTITY NOT NULL,
+	primer_nombre VARCHAR(200) NOT NULL,
+	segundo_nombre VARCHAR(200) NOT NULL,
+	primer_apellido VARCHAR(200) NOT NULL,
+	segundo_apellido VARCHAR(200) NOT NULL,
+	telefono VARCHAR(200) NOT NULL,
+	correo_electronico VARCHAR(200) NOT NULL,
+	direccion VARCHAR(200) NOT NULL,
+	[login] VARCHAR(100)  NOT NULL UNIQUE,
+	[password] varchar (50) NOT NULL,
+	fecha_de_registro DATETIME NOT NULL,
+	fecha_de_actualizacion DATE NOT NULL,
+	activo BIT,
 );
-go
+GO
 
-INSERT INTO Usuarios (Nombre, Loggin, Pass, FechaRegistro, IdUsuarioActualizacion, FechaActualizacion, Activo)
-VALUES
-    ('Margel', 'rekedomina', 'rekedomina', GETDATE(), 1, GETDATE(), 1),
-    ('Josue', 'peludito', 'peludito', GETDATE(), 1, GETDATE(), 1),
-    ('Hans', 'chimpirilin', 'chimpirilin', GETDATE(), 1, GETDATE(), 1);
-go
+CREATE TABLE deudor (
+	id int primary key identity NOT NULL,
+	primer_nombre VARCHAR(200) NOT NULL,
+	segundo_nombre VARCHAR(200) NOT NULL,
+	primer_apellido VARCHAR(200) NOT NULL,
+	segundo_apellido VARCHAR(200) NOT NULL,
+	telefono VARCHAR(200) NOT NULL,
+	correo_electronico VARCHAR(200) NOT NULL,
+	direccion VARCHAR(200) NOT NULL,
+	fecha_de_registro DATETIME NOT NULL,
+	fecha_de_actualizacion DATE NOT NULL,
+	deuda MONEY,
+	activo BIT,
+);
+GO
+
+-- Actualizar estos valores con el nuevo schema
+
+-- INSERT INTO Usuarusuario_de_aplicacionios (Nombre, Loggin, Pass, FechaRegistro, IdUsuarioActualizacion, FechaActualizacion, Activo)
+-- VALUES
+--     ('Margel', 'rekedomina', 'rekedomina', GETDATE(), 1, GETDATE(), 1),
+--     ('Josue', 'peludito', 'peludito', GETDATE(), 1, GETDATE(), 1),
+--     ('Hans', 'chimpirilin', 'chimpirilin', GETDATE(), 1, GETDATE(), 1);
+-- go
 
 --Tablas Faltantes:  Productos, 
 
